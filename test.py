baf0cb8fd299ae0edfd2dc4778064366f4ff384f
yang <634228743@qq.com>
2017-07-28 21:18:17 +0800

    network

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import load
 training_data, validation_data, test_data = load.load_data_wrapper()
 training_data = list(training_data)
 import nw
+import  network
 
 
 net = nw.Network([784, 30, 10])
